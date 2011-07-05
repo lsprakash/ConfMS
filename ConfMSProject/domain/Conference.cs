d48da4aa69f8d84380a7a9baf761b7ee0d00e559
Shivasubramanian A <shivasa@thoughtworks.com>
2011-07-05 15:55:56 +0530

    #2 [Shiva/Madhu] Added Conference domain model and test

---
+++ ConfMSProject/domain/Conference.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ConfMSProject.domain
+{
+    public class Conference
+    {
+        public string Title { get; set; }
+        public DateTime StartTime { get; set; }
+        public int Duration { get; set; }
+
+        public Conference(string title, DateTime startTime, int duration)
+        {
+            Title = title;
+            StartTime = startTime;
+            Duration = duration;
+        }
+    }
+}
---
+++ ConfMSTest/domain/ConferenceTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using ConfMSProject.domain;
+using NUnit.Framework;
+
+namespace ConfMSTest.domain
+{
+    [TestFixture]
+    class ConferenceTest
+    {
+        [Test]
+        public void ShouldCreateAConferenceObject()
+        {
+            string title = "RubyConf";
+            DateTime startTime = new DateTime(2011, 7, 5, 9, 0, 0);
+            int durationInHours = 8; //TODO: replace primitive with Duration class.
+            Conference conference = new Conference(title, startTime, durationInHours);
+
+            Assert.AreEqual(title, conference.Title);
+            Assert.AreEqual(startTime, conference.StartTime);
+            Assert.AreEqual(durationInHours, conference.Duration);
+        }
+    }
+}
