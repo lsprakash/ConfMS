bfb44d6913b88296ca776e9e9d06a576484d0758
Gurpreet Luthra <gluthra@thoughtworks.com>
2011-07-05 15:35:35 +0530

    #3 [Gurpreet/Dam] Added Session domain objects/tests and Title type

---
+++ ConfMSProject/domain/ConfSession.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ConfMSProject.domain
+{
+    public class ConfSession
+    {
+
+        public ConfSession(string sessionTitle)
+        {
+            SessionTitle = new Title(sessionTitle);
+        }
+
+        public Title SessionTitle { get; set; }
+
+        public string Description { get; set; }
+    }
+}
