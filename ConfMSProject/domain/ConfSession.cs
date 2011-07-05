0bed60d275001598bbad0f73b3bbdb79a2fe9557
Gurpreet Luthra <gluthra@thoughtworks.com>
2011-07-05 20:17:04 +0530

    #3 [Gurpreet/Dam] Added Conference Session objects with repository save/find code. Added DLLs too.

--- ConfMSProject/domain/ConfSession.cs
+++ ConfMSProject/domain/ConfSession.cs
@@ -5,13 +5,21 @@ namespace ConfMSProject.domain
     public class ConfSession
     {
 
+        public ConfSession()
+        {
+         //Nothing to do
+            
+        }
+
         public ConfSession(string sessionTitle)
         {
             SessionTitle = new Title(sessionTitle);
         }
 
-        public Title SessionTitle { get; set; }
+        public virtual Title SessionTitle { get; set; }
 
-        public string Description { get; set; }
+        public virtual string Description { get; set; }
+        
+        public virtual string Id { get; set; }
     }
 }
---
+++ ConfMSTest/repository/SessionRepositoryTest.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text;
+using ConfMSProject.domain;
+using ConfMSProject.repository;
+using NUnit.Framework;
+
+namespace ConfMSTest.repository
+{
+    [TestFixture]
+    class SessionRepositoryTest : NHibernateInMemoryTestFixtureBase
+    {
+
+        [Test]
+        public void ShouldSaveConfSessionToDB()
+        {
+            var confSession = new ConfSession("Test Session");
+            confSession.Description = "Some long description.. little long only";
+
+            var repository = new ConfSessionRepository();
+            repository.Save(confSession);
+
+            var sessions = repository.FindByTitle("Test");
+            var returnedSession = sessions[0];
+            Assert.AreEqual(returnedSession, confSession);
+
+            var sessions2 = repository.FindByTitle("ghhg");
+            Assert.True(sessions2.Count==0);
+
+        }
+
+    }
+}
